5252b9741364a5af3c562e89de7d5085cd2a48ee
stuser <stuser@192.168.1.33>
2019-07-03 13:30:22 -0400

    tic tac toe trouble day 3 bbbbbbbbbbbbbbbbbbbb

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -14,13 +14,13 @@ public class ticTacToe {
 		}
 		while(keepGoing == true) {
 			if(letter.equalsIgnoreCase("X")) {
-				System.out.println("Player 1, state the row that you want to put your X. Rows go left to right.");
+				System.out.println("Player 1, state the row that you want to put your X. Rows go from left to right.");
 			}
 			else {
-				System.out.println("Player 2, state the row that you want to put your O. Rows go left to right.");
+				System.out.println("Player 2, state the row that you want to put your O. Rows go from left to right.");
 			}
 			int row = scan.nextInt();
-			System.out.println("Now, state the column. Columns go up to down.");
+			System.out.println("Now, state the column. Columns go from up-down.");
 			int column = scan.nextInt();
 			board[row][column] = ""+letter+"";
 			System.out.println(" "+board[0][0]+" | "+board[1][0]+" | "+board[2][0]+" ");
@@ -28,6 +28,7 @@ public class ticTacToe {
 			System.out.println(" "+board[0][1]+" | "+board[1][1]+" | "+board[2][1]+" ");
 			System.out.println("-----------");
 			System.out.println(" "+board[0][2]+" | "+board[1][2]+" | "+board[2][2]+" ");
+			checkWin(board,keepGoing);
 			if(letter.equalsIgnoreCase("X")) {
 				letter = "O";
 				continue;
@@ -38,8 +39,74 @@ public class ticTacToe {
 		}
 		scan.close();
 	}
-	public static void checkWin(String[][] board, boolean keepGoing) {
-		if()
-		keepGoing = false;
+
+	public static boolean checkWin(String[][] board, boolean keepGoing) {
+		if(board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		if(board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		if(board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		if(board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		if(board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		if(board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		if(board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") {
+			System.out.println("Game over! Player 1 wins.");
+			keepGoing = false;
+		}
+		if(board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O") {
+			System.out.println("Game over! Player 2 wins.");
+			keepGoing = false;
+		}
+		return keepGoing;
+	}
+	
+	public static boolean checkTie(String[][] board, boolean keepGoing) {
+		for(int y=0;y<3;y++) {
+			if(board[y][y] != null) {
+				System.out.println("Game over! It's a tie.");
+				keepGoing = false;
+			}
+		}
+		return keepGoing;
 	}
 }
