fdbc19d790555caa16d86bcee55c5394c57bc0b1
stuser <stuser@192.168.1.33>
2019-07-04 11:16:09 -0400

    RPG Game prolougue end day 4 bbbbbbbbbbbbbbbbbbbbb

--- src/rpgGame.java
+++ src/rpgGame.java
@@ -6,6 +6,8 @@ public class rpgGame {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		Random rand = new Random();
+		String[] inv = new String[10];
+		int i = 0; //i is number of items in inv.
 		int rpsComNum = rand.nextInt(3);
 		String rpsComChoice = null;
 		if(rpsComNum == 0) {
@@ -71,16 +73,59 @@ public class rpgGame {
 		else if(firstChoice.equalsIgnoreCase("MEMES")) {
 			System.out.println("* You're too tired to go downstairs anyway.");
 			System.out.println("* You go back to your computer to watch memes.");
+			System.out.println("* ...");
+			System.out.println("* ...");
+			System.out.println("* ...");
 			System.out.println("* You're hungry. You go downstairs.");
 		}
+		else if(firstChoice.equalsIgnoreCase("TAKE BATTERIES")) {
+			System.out.println("* You take the dead batteries from the machine. You probably don't need them anymore, but you decided to take them anyway.");
+			System.out.println("* You got 2 (dead) E-Cell Batteries.");
+			inv[i] = "Dead Batteries";
+			i++;
+			System.out.println("* You go downstairs.");
+		}
 		else {
-			System.out.println("* As punishment for typing incorrectly, you go downstairs, even though you don't want to.");
+			System.out.println("* As punishment for typing incorrectly, you go downstairs, even though you probably didn't want to.");
 		}
 		if(firstChoice.equalsIgnoreCase("GET BATTERIES")) {
 			System.out.println("* You walk downstairs, and wonder what would've happened if you chose to stay and watch m e e m z.");
 		}
 		else {
 			System.out.println("* You walk downstairs.");
+		}
+		System.out.println("* Your dog starts barking. He probably needs a walk.");
+		System.out.println("* You take your dog for a walk. You don't really have a choice.");
+		System.out.println("* Apparently, the one who made this is too lazy to put in an if statement for you to make a choice in the first place.");
+		System.out.println("* You go outside.");
+		System.out.println("* It's 3 in the morning.");
+		System.out.println("* Before you go back inside, your dog opens up a rift in space and time and goes in.");
+		System.out.println("* Oh well. Guess that sucks. You go back inside your house.");
+		System.out.println("* You go to your kitchen, since you're (probably) hungry.");
+		System.out.println("* You try to open the refrigerator.");
+		System.out.println("* You miss.");
+		System.out.println("* You try to open the refrigerator again. This time it opens. You get a drink.");
+		System.out.println("* You miss. You get the cabbage.");
+		System.out.println("* You try to put back the cabbage.");
+		System.out.println("* You miss. It lands on the floor with a resounding thud.");
+		System.out.println("* A feeling of guilt surrounds you. Oh well.");
+		System.out.println("* You grab the box next to the fridge. You get...");
+		System.out.println("* A   S T A L E   C R A C K E R .");
+		System.out.println("* Stale Cracker has been added to your inventory.");
+		inv[i] = "Stale Cracker";
+		i++;
+		System.out.println("* For a reason that would take way to long to think of, you decide to go outside.");
+		System.out.println("* Among the hills, you see a cave. You grab your iron pickaxe and walk inside.");
+		System.out.println("* To find diamonds, it's always recommended to dig straight down. So you do that.");
+		System.out.println("* Uh oh. You're stuck.");
+		System.out.println("* And, since the one who created this was forced by hogan luckins for the MC to fall into an ice cube with like a pond with a 2ft diameter, you do that.");
+		System.out.println("[probably many years later]");
+		System.out.println("P R O L O U G U E   C O M P L E T E");
+		System.out.println("* Always remember that after a chapter is complete, type anything to go on to the next chapter.");
+		String nextChapter = scan.next();
+		System.out.println("Continuing to next chapter.");
+		for(int c=0;c<10;c++) {
+			System.out.println();
 		}
 		
 	}
