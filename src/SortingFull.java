fb1ff6cc83553be05034cbf2f66ef4e631500bca
SummerTech <stusr@summertech.net>
2019-07-11 13:31:46 -0400

    insertionSort day 5 cccccccccccc

---
+++ src/SortingFull.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+public class SortingFull{
+    public static void main(String[] args){
+        int[] numList = new int[10];
+        Random rand = new Random();
+        for(int x=0; x<numList.length; x++){
+            numList[x] = rand.nextInt(100);
+        }
+        System.out.println("U N S O R T E D   L I S T");
+        for(int y=0; y<numList.length; y++){
+            System.out.print(numList[y]);
+            System.out.print(", ");
+        }
+        System.out.println();
+        System.out.println();
+
+        numList = insertionSort(numList);
+
+        System.out.println("S O R T E D   L I S T");
+        for(int y=0; y<numList.length; y++){
+            System.out.print(numList[y]);
+            System.out.print(", ");
+        }
+    }
+
+    public static int[] bubbleSort(int[] numList){
+        //goes through the list and if a number is greater than the next number, it swaps.
+        for(int a=0; a<numList.length; a++){
+            for(int z=0; z<(numList.length-1); z++){
+                if(numList[z] > numList[z+1]){
+                    //swap the places of both numbers
+                    int temp = numList[z];
+                    numList[z] = numList[z+1];
+                    numList[z+1] = temp;
+                }
+            }
+        }
+        return numList;
+    }
+    
+    public static int[] selectionSort(int[] numList){
+        //goes through the list from a selected number to find a number smaller than one that is selected, and swaps with it.
+        for(int b=0; b<numList.length; b++){
+            int lowNum = b;
+            for(int c=b; c<numList.length; c++){
+                if(numList[lowNum] > numList[c]){
+                    lowNum = c;
+                }
+            }
+            int temp = numList[lowNum];
+            numList[lowNum] = numList[b];
+            numList[b] = temp;
+        }
+        return numList;
+    }
+
+    public static int[] insertionSort(int[] numList){
+        //assumes first number is sorted and other numbers go left or right compared to other numbers.
+        for(int d=1; d<(numList.length); d++){
+
+            for(int e=0; e<numList.length; e++){
+                System.out.print(numList[e]);
+                System.out.print(", ");
+            }
+            System.out.println();
+
+            int f = 0;
+            while(numList[d-f-1] > numList[d-f]){
+                int temp = numList[d-f-1];
+                numList[d-f-1] = numList[d-f];
+                numList[d-f] = temp;
+                f--;
+            }
+        }
+        System.out.println();
+        return numList;
+    }
+}
